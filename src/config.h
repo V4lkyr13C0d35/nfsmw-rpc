acb8e6e89b3b6488b48451dac19d45bf8b000525
13B <97484388+V4lkyr13C0d35@users.noreply.github.com>
2025-11-01 21:52:38 +0800

    Update APP_ID in config.h

--- src/config.h
+++ src/config.h
@@ -1,6 +1,7 @@
-#define APP_ID "864006876319186995"
+#define APP_ID "1434178200543563907"
 #define IMG_KEY "logo"
 #define IMG_TXT "Need for Speed: Most Wanted"
 #define UPD_INTVL 5000
 
 #define IN_MENU "Main Menu" // ANY menu
+
